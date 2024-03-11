77c21bdf4aa6f65311a7e196c82eff2e15b2a0e5
Chris Cummings <chriscummings@Chriss-MacBook-Pro.local>
2024-03-10 19:07:22 -0600

    formatting & portfolio redirect

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,3 +3,17 @@ for (let counter = 0; counter < numberOfBlogs; counter++) {
   let blog = localstorage.getItem(blog + counter);
   console.log(blog);
 }
+
+const lightDarkEl = $("#lightdark-btn");
+let isDark = true;
+
+lightDarkEl.on("click", function () {
+  if (isDark) {
+    //selecting element of body calling the css file, use css selectors to select DOM elements
+    $("body").css({ "background-color": "#d9e9e8", color: "#1a1a1a" });
+    isDark = !isDark;
+  } else {
+    $("body").css({ "background-color": "#1a1a1a", color: "#d9e9e8" });
+    isDark = !isDark;
+  }
+});
--- assets/js/form.js
+++ assets/js/form.js
@@ -30,3 +30,17 @@ blogForm.addEventListener("submit", (e) => {
   console.log(localStorage);
   // Format content into a JSON Object to put in local storage
 });
+
+const lightDarkEl = $("#lightdark-btn");
+let isDark = true;
+
+lightDarkEl.on("click", function () {
+  if (isDark) {
+    //selecting element of body calling the css file, use css selectors to select DOM elements
+    $("body").css({ "background-color": "#d9e9e8", color: "#1a1a1a" });
+    isDark = !isDark;
+  } else {
+    $("body").css({ "background-color": "#1a1a1a", color: "#d9e9e8" });
+    isDark = !isDark;
+  }
+});
